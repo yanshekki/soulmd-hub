66c63be43ded349e72040611d673850b4e4ba499
Ki <yanshekki@gmail.com>
2026-05-25 18:04:54 +0800

    update

--- public_html/invoice.php
+++ public_html/invoice.php
@@ -92,6 +92,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/images/icon-192x192.png" sizes="192x192" type="image/png">
     <title>Invoice <?= $invoiceNumber ?> - SoulMD Hub</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
